4fda65a2edb49132e29f03342a34ec16779513ff
Bryan Fernandes <bryan.efernandes@gmail.com>
2022-01-25 17:32:42 -0300

    Requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui 
+function getSpeciesByIds(...ids) {
+  if (ids.length === 0) {
+    return [];
+  }
+
+  if (ids.length === 1) {
+    const animalInfo = [];
+
+    const idValidation = data.species.find((idValue) => idValue.id === ids[0]);
+
+    animalInfo.push(idValidation);
+
+    return animalInfo;
+  }
+
+  return data.species.filter((animalId, index) => animalId.id === ids[index]);
 }
+
+console.log(getSpeciesByIds('01422318-ca2d-46b8-b66c-3e9e188244ed'));
 
 module.exports = getSpeciesByIds;
