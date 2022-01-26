4e5e21122e8b4b2643677b1e7f88016256cc11a7
Bryan Fernandes <bryan.efernandes@gmail.com>
2022-01-26 16:08:25 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,28 @@
 const data = require('../data/zoo_data');
 
+const animalsPopulation = {
+  lions: 4,
+  tigers: 2,
+  bears: 3,
+  penguins: 4,
+  otters: 4,
+  frogs: 2,
+  snakes: 2,
+  elephants: 4,
+  giraffes: 6,
+};
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return animalsPopulation;
+  }
+  if (!animal.sex) {
+    return data.species.find((value) => value.name === animal.specie).residents
+      .length;
+  }
+  return data.species
+    .find((value) => value.name === animal.specie)
+    .residents.filter((value) => value.sex === animal.sex).length;
 }
 
 module.exports = countAnimals;
