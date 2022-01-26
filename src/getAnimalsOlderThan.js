acb002c85929d5ed76abbda092fc74c02a257422
Bryan Fernandes <bryan.efernandes@gmail.com>
2022-01-26 08:28:30 -0300

    Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const animalInfo = data.species.find((animalToFind) => animalToFind.name === animal);
+  return animalInfo.residents.every((value) => value.age > age);
 }
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -18,6 +18,4 @@ function getSpeciesByIds(...ids) {
   return data.species.filter((animalId, index) => animalId.id === ids[index]);
 }
 
-console.log(getSpeciesByIds('01422318-ca2d-46b8-b66c-3e9e188244ed'));
-
 module.exports = getSpeciesByIds;
