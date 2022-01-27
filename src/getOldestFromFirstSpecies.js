6b6d6d75fea274b968b4478eef9dd225d05a8487
Bryan Fernandes <bryan.efernandes@gmail.com>
2022-01-27 13:24:25 -0300

    Requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const firstResposible = data.employees.find((value) => value.id === id)
+    .responsibleFor[0];
+
+  const animalVerify = data.species.find((animal) => animal.id === firstResposible).residents;
+
+  const residentsAge = animalVerify
+    .map((value) => value.age)
+    .sort((a, b) => a - b);
+
+  const oldestAnimal = residentsAge[residentsAge.length - 1];
+
+  const oldestAnimalObj = animalVerify.find((value) => value.age === oldestAnimal);
+
+  return [oldestAnimalObj.name, oldestAnimalObj.sex, oldestAnimalObj.age];
 }
 
 module.exports = getOldestFromFirstSpecies;
